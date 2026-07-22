475eae55adeb70f490bdb2b0f2922cbe042429f3
RickV22 <valienter69@gmail.com>
2026-07-22 14:09:10 -0500

    aws deploy

---
+++ FrontendV2/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://3.17.182.157:8000',
+  googleClientId: '248566846005-8fc3f989r904dsr8b8eghnc1b122uhm7.apps.googleusercontent.com',
+};
